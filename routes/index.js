66e6d8dd1feec9b7d54b29c543e5dc1c24aa2fc5
khald <abv.khallid@mail.ru>
2022-06-03 15:01:42 +0300

    project template

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const {
+  Router
+} = require('express')
+const router = Router()
+
+router.use(require('./carts.route'))
+router.use(require('./products.route'))
+router.use(require('./categories.route'))
+router.use(require('./brands.route'))
+router.use(require('./reviews.route'))
+
+module.exports = router
